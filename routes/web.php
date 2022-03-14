57a65a29fb4378e2e3fb49b68f8c317c576e7384
Angeloamenta <angeloamenta7@gmail.com>
2022-03-14 15:11:01 +0100

    aggiunte rotte in web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
 Auth::routes();
@@ -26,5 +26,11 @@ Route::middleware('auth')
     ->group(function(){
         Route::get('/', 'HomeController@index')->name('home');
     });
+
+
+Route::get("{any?}", function () {
+    return view("guest.home");
+})->where("any", ".*");
+    
 
 
